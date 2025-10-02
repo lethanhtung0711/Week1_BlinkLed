bcc59f362f008402b677177a833dccd48f09901d
lethanhtung0711 <lethanhtung_t67@hus.edu.vn>
2025-10-02 22:26:48 +0700

    Update platformio.ini and main.cpp with new changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <OneButton.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 5      
+#define BUTTON_PIN 4   
+OneButton button(BUTTON_PIN, true); 
+
+bool ledState = false;      
+bool blinkMode = false;      
+unsigned long lastToggle = 0;
+
+void toggleLED() {
+  blinkMode = false;
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+}
+void setBlinkMode() {
+  blinkMode = !blinkMode;
+  if (!blinkMode) {
+    digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  button.attachClick(toggleLED);
+  button.attachDoubleClick(setBlinkMode);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  button.tick();
+  if (blinkMode) {
+    if (millis() - lastToggle > 1000) {  
+      lastToggle = millis();
+      ledState = !ledState;
+      digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+    }
+  }
 }
